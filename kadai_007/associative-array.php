0422d467f6fb43901fd1481b892cb51ad851bce3
honda-teoria <162296715+honda-teoria@users.noreply.github.com>
2024-07-16 17:50:58 +0900

    初回コミット

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>PHP基礎編課題</title>
+  <meta charset="UTF-8">
+</head>
+
+<body>
+  <p>
+    <?php
+    $goods = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+    print_r($goods);
+    ?>
+  </p>
+</body>
+
+</html>
