18a4e2fbdc724263fd3e230286457c28a7e296ec
honda-teoria <162296715+honda-teoria@users.noreply.github.com>
2024-08-22 13:04:06 +0900

    初回コミット

---
+++ kadai_web_app/complete.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title></title>
+  <meta>
+</head>
+
+<body>
+  <h2>登録が完了しました。</h2>
+  <p>社員情報がデータベースに保存されました。</p>
+  <button onclick="location.href='form.php';">戻る</button>
+</body>
+
+</html>
